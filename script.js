41ce145327f75b74e4ec2760bb73bc77db5fd661
Filippo Pilia <fippopia@gmail.com>
2026-04-01 09:48:19 +0200

    Primo commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Log per verificare che il file sia caricato correttamente
+console.log("Applicazione Etilometro caricata con successo.");
+
+// Qui andrà tutta la logica per il calcolo del tasso alcolemico
+document.addEventListener('DOMContentLoaded', () => {
+    // Il DOM è completamente caricato ed analizzato
+    console.log("DOM pronto. Inizializzazione applicazione...");
+});
